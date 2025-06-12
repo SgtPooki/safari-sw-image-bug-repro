0e441d86dd8c7ae13ffae0d021006e27928459e9
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-06-12 11:46:11 -0400

    feat: initial config

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# safari-sw-image-bug-repro
+# Safari Service Worker Bug Repro
+
+## 🧪 What this shows
+
+It shows that the bug on inbrowser.link is not related to Safari, because it works in Safari with this repo.
+
+## 📦 How to test
+
+Visit https://sgtpooki.github.io/safari-sw-bug-repro/
+
+Click the links to see the different ways to serve the image.
+
+You can also go to https://sgtpooki.github.io/safari-sw-bug-repro/image-sw-stream, which will first load HTML (prior to sw activation), and then the page will reload and the image will be served from the SW.
+
+
+## Local testing
+
+Run `npx http-server -p 8998 .` and visit http://localhost:8998/ in Safari.
---
+++ sw.js
@@ -0,0 +1,150 @@
+// @ts-nocheck
+// import unixfs from helia and memory blockstore
+import { IDBBlockstore } from 'https://esm.sh/blockstore-idb@2.0.3'
+import { unixfs } from 'https://esm.sh/@helia/unixfs@5.0.3'
+import { CID } from 'https://esm.sh/multiformats@13.3.7/cid'
+
+const blockstore = new IDBBlockstore('store-path')
+const fs = unixfs({ blockstore })
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
+})
+
+async function ensureContentIsLoaded() {
+  const cid = CID.parse('bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi')
+  await blockstore.open()
+
+  // if (blockstore.has(cid)) {
+  //   console.log('CID already in blockstore')
+  //   return
+  // }
+
+  const response = await fetch('https://trustless-gateway.link/ipfs/bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi?format=raw', {
+    headers: {
+      Accept: 'application/vnd.ipld.raw',
+    },
+  })
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+
+  // Convert ArrayBuffer to Uint8Array
+  const arrayBuffer = await response.arrayBuffer()
+  const uint8Array = new Uint8Array(arrayBuffer)
+
+  // Add raw bytes to blockstore directly
+  await blockstore.put(cid, uint8Array)
+  console.log('CID added to blockstore:', cid)
+}
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url)
+  if (url.pathname === '/') {
+    // don't handle the root path
+    return
+  }
+  const cid = CID.parse('bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi')
+  if (url.pathname === '/test-ready') {
+    console.log('Test ready endpoint hit')
+    event.respondWith(new Response('ready', {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    }))
+    return
+  }
+
+  if (url.pathname === '/unregister') {
+    console.log('Unregistering SW')
+    event.waitUntil(self.registration.unregister())
+    return
+  }
+
+  if (url.pathname === '/image-sw-blob') {
+    console.log('Fetching image-sw')
+
+    // Create a promise that will resolve with the response
+    const responsePromise = (async () => {
+      try {
+        await ensureContentIsLoaded()
+
+        const chunks = []
+        for await (const chunk of await fs.cat(cid)) {
+          chunks.push(chunk)
+        }
+
+        return new Response(new Blob(chunks), {
+          status: 200,
+          headers: {
+            'Content-Type': 'image/jpeg',
+            'x-cid': cid.toString(),
+          }
+        })
+      } catch (error) {
+        console.error('Error:', error)
+        return new Response('Error: ' + error.message, {
+          status: 500,
+          headers: {
+            'Content-Type': 'text/plain',
+          },
+        })
+      }
+    })()
+
+    // Respond with the promise
+    event.respondWith(responsePromise)
+    return
+  }
+
+  if (url.pathname === '/image-sw-stream') {
+    console.log('Fetching image-sw')
+
+    // Create a promise that will resolve with the response
+    const responsePromise = (async () => {
+      try {
+        await ensureContentIsLoaded()
+
+        const chunks = []
+        for await (const chunk of await fs.cat(cid)) {
+          chunks.push(chunk)
+        }
+
+        const stream = new ReadableStream({
+          async start(controller) {
+            for await (const chunk of chunks) {
+              controller.enqueue(chunk)
+            }
+            controller.close()
+          }
+        })
+
+        return new Response(stream, {
+          status: 200,
+          headers: {
+            'Content-Type': 'image/jpeg',
+            'x-cid': cid.toString(),
+          }
+        })
+      } catch (error) {
+        console.error('Error:', error)
+        return new Response('Error: ' + error.message, {
+          status: 500,
+          headers: {
+            'Content-Type': 'text/plain',
+          },
+        })
+      }
+    })()
+
+    // Respond with the promise
+    event.respondWith(responsePromise)
+    return
+  }
+
+})
